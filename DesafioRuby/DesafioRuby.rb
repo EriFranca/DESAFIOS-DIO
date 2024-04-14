dc6ffd0e6734ae5e1f8ea9beac61e97126d20aad
Eri França <87365333+EriFranca@users.noreply.github.com>
2024-04-14 17:38:49 -0300

    desafio rb

---
+++ DesafioRuby/DesafioRuby.rb
@@ -0,0 +1,15 @@
+#Solicite o nome do usuário
+print "Digite seu Nome: "
+nome = gets.chomp
+
+#Solicite o Sobrenome do usuário
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+
+#Solicite a idade do usuário
+print "Digite sua Idade: "
+idade = gets.chomp
+
+#Apresenta os dados na tela
+puts "Olá #{nome} #{sobrenome}"
+puts "Sua Idade é #{idade}"
